d33e4eda5240bd2ecad8b2d1a2f88864ef2fdde4
AlexDgebuadze <aleksandre.dgebuadze@geolab.edu.ge>
2021-04-11 20:02:58 +0400

    refinement

---
+++ Main/ind.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="css/style.css">
+
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+
+    <link href="https://fonts.googleapis.com/css?family=poppins" rel="stylesheet"> <!--Google ფონტები Nav-Bar-სთვის-->
+
+    <link href="https://fonts.googleapis.com/css2?family=Knewave&display=swap" rel="stylesheet"> <!--EXPLORE GEORGIA-სთვის-->
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+
+
+    <title>GeoTraveler</title>
+
+</head>
+
+<body>
+
+asasasaasas
+
+</body>
+
+</html>
